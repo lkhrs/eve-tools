b1633be8c11a073e13782baf1dd7c9d451ae881a
Luke Harris <luke@lkhrs.com>
2023-01-12 15:02:58 -0600

    fix auto-pr

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "lkhrs/eve-tools:main"
+    [String]$upstream = "lkhrs/eve-tools:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
